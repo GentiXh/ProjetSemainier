fc1852401f2b17b55ce79d5026e7d693612db502
anoukaltmeyer <102285978+anoukaltmeyer@users.noreply.github.com>
2022-03-24 16:51:41 +0100

    Update Recette.java

--- Recette.java
+++ Recette.java
@@ -1,11 +1,7 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-
 public class Recette extends JFrame {
 	
 	public Recette(){
-		JPanel fond = new JPanel();
+		JPanel fond = new JPanel(new BorderLayout());
         JLabel titre = new JLabel();
         JTabbedPane jTabbedPane1 = new JTabbedPane();
         JPanel francais = new JPanel();
@@ -14,16 +10,22 @@ public class Recette extends JFrame {
         JPanel fastfood = new JPanel();
         JPanel petitdej = new JPanel();
         
+        this.setSize(700,700);
+
+        
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(700, 700));
         
         fond.setBackground(new Color(204, 255, 204));
-
+		this.add(fond);
+		
         titre.setBackground(new Color(255, 255, 255));
         titre.setFont(new Font("Stencil", 0, 48)); 
         titre.setText("RECETTES");
-
+		fond.add(titre, BorderLayout.NORTH);
+		
         jTabbedPane1.setBackground(new Color(255, 255, 255));
+        fond.add(jTabbedPane1, BorderLayout.SOUTH);
 
         francais.setBackground(new Color(255, 255, 255));
         
@@ -44,3 +46,4 @@ public class Recette extends JFrame {
 	
 	
 }
+
