13d8bd9e06d9a2cc25e7aaf22bfec718850cadce
anoukaltmeyer <102285978+anoukaltmeyer@users.noreply.github.com>
2022-03-24 18:27:56 +0100

    Update AllergiePage2.java

--- AllergiePage2.java
+++ AllergiePage2.java
@@ -5,78 +5,87 @@ import java.awt.event.*;
 public class AllergiePage2 extends JPanel{
 	
 	public boolean valide= false;
+	
     public AllergiePage2() {
+			
 		JButton jButton1 = new JButton();
-        JPanel contenu = new JPanel();
+        JPanel contenu = new JPanel(new GridBagLayout());
         JCheckBox glutenBox = new JCheckBox();
         JCheckBox lactoseBox = new JCheckBox();
         JCheckBox arachideBox = new JCheckBox();
-        JCheckBox foodmapBox = new JCheckBox();
+        JCheckBox fodmapBox = new JCheckBox();
         JCheckBox oeufBox = new JCheckBox();
         JCheckBox autresBox = new JCheckBox();
+
+		this.setSize(700,700);
+
 
         jButton1.setText("jButton1");
 
         setBackground(new Color(204, 204, 255));
-
+		
         contenu.setBackground(new Color(204, 204, 255));
+        this.add(contenu);
 
         glutenBox.setFont(new Font("Stencil", 0, 36)); 
         glutenBox.setText("Gluten");
+        glutenBox.setBackground(new Color(204, 204, 255));
+        contenu.add(glutenBox);
         
-        glutenBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                glutenBoxActionPerformed(evt);
-            }
-        });
+        glutenBox.addActionListener(new EcouteurAllergie2());
 
         lactoseBox.setFont(new Font("Stencil", 0, 36)); 
         lactoseBox.setText("Lactose");
-        lactoseBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                lactoseBoxActionPerformed(evt);
-            }
-        });
+        contenu.add(lactoseBox);
+        lactoseBox.setBackground(new Color(204, 204, 255));
+        lactoseBox.addActionListener(new EcouteurAllergie2());
+       
 
         arachideBox.setFont(new Font("Stencil", 0, 36));
         arachideBox.setText("Arachide");
+        contenu.add(arachideBox);
+        arachideBox.setBackground(new Color(204, 204, 255));
 
-        foodmapBox.setFont(new Font("Stencil", 0, 36)); 
-        foodmapBox.setText("Foodmap");
-        foodmapBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                foodmapBoxActionPerformed(evt);
-            }
-        });
+        fodmapBox.setFont(new Font("Stencil", 0, 36)); 
+        fodmapBox.setText("Fodmap");
+        contenu.add(fodmapBox);
+		fodmapBox.setBackground(new Color(204, 204, 255));
+		
+		fodmapBox.addActionListener(new EcouteurAllergie2());
 
         oeufBox.setFont(new Font("Stencil", 0, 36));
         oeufBox.setText("Oeuf");
-        oeufBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                oeufBoxActionPerformed(evt);
-            }
-        });
+        contenu.add(oeufBox);
+        oeufBox.setBackground(new Color(204, 204, 255));
+        
+		oeufBox.addActionListener(new EcouteurAllergie2());    
 
         autresBox.setFont(new Font("Stencil", 0, 36)); 
         autresBox.setText("Autres");
+        contenu.add(autresBox);
+        autresBox.setBackground(new Color(204, 204, 255));
+
+		
 		
 	}
 	
-	 private void glutenBoxActionPerformed(java.awt.event.ActionEvent evt) {                                          
+	
+	/* private void glutenBoxActionPerformed(ActionEvent evt) {                                          
         // TODO add your handling code here:
     }   
     
-    private void lactoseBoxActionPerformed(java.awt.event.ActionEvent evt) {                                           
+    private void lactoseBoxActionPerformed(ActionEvent evt) {                                           
         // TODO add your handling code here:
     }                                          
 
-    private void oeufBoxActionPerformed(java.awt.event.ActionEvent evt) {                                        
+    private void oeufBoxActionPerformed(ActionEvent evt) {                                        
         // TODO add your handling code here:
     }                                       
 
-    private void foodmapBoxActionPerformed(java.awt.event.ActionEvent evt) {                                           
+    private void foodmapBoxActionPerformed(ActionEvent evt) {                                           
         // TODO add your handling code here:
-    }           
+    }   */        
     
 	
 }
+
