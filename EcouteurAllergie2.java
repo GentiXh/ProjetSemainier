ba7f15b02698d0d2277c68166cded4705070008f
anoukaltmeyer <102285978+anoukaltmeyer@users.noreply.github.com>
2022-03-31 14:56:52 +0200

    Add files via upload

---
+++ EcouteurAllergie2.java
@@ -0,0 +1,42 @@
+import java.awt.event.*;
+import java.util.*;
+
+public class EcouteurAllergie2 implements ActionListener{
+	
+	private Allergie fenetre;
+	int allergene;
+		
+	//constructeur
+	public EcouteurAllergie2(Allergie fen, int val){
+		fenetre=fen;
+		allergene = val;
+	}
+	
+	public void actionPerformed(ActionEvent evt) {
+        if (allergene == 1){
+			fenetre.gluten=true;
+		}
+		if (allergene == 2){
+			fenetre.lactose=true;
+		}
+		if (allergene == 3){
+			fenetre.arachide=true;
+		}
+		if (allergene == 4){
+			fenetre.fodmap=true;
+		}
+		if (allergene == 5){
+			fenetre.oeuf=true;
+		}
+		if (allergene == 6){
+			fenetre.autres=true;
+		}
+		
+		
+		
+    }
+  
+    
+		
+		
+}
