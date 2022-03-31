4cc5ecae4ca78b31de3419f0ea9b42f025796980
Meyane <102722649+Meyane@users.noreply.github.com>
2022-03-31 09:46:10 +0200

    Update RegimeAlimentaire.java

--- RegimeAlimentaire.java
+++ RegimeAlimentaire.java
@@ -16,3 +16,5 @@ public class RegimeAlimentaire {
 //Lorsque l'on appuie sur bouton Omnivore 0 ou Vegetarien 1 ou Vegan 2; cela enregistre the number et à partir du number enregistré 
 //il faut acceder à la base de donnée idIngredient que l'on regarde quels ingrédients correspondent niveau boolean au regime
 //spécifié puis qu'on les enregistre dans les linked List  
+
+//du coup omnivore, vegetarien et vegan sont des int !
