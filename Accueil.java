1694137153b3ddc61a2a172d2f3dcf7358ca03f8
anoukaltmeyer <102285978+anoukaltmeyer@users.noreply.github.com>
2022-04-05 09:03:41 +0200

    Update Accueil.java

--- Accueil.java
+++ Accueil.java
@@ -14,31 +14,35 @@ public class Accueil extends JFrame{
         //on definit le nom de la fenetre
         super("Happy frigo");
 
-        //Dimensions de la fenetre graphique et fermeture
-        this.setSize(700,700);
+        //récuperer la taille de l'écran + mettre fenetre au centre de l'ecran  + fermeture
+	this.pack();
+	Dimension tailleEcran = Toolkit.getDefaultToolkit().getScreenSize();
+	int height = tailleEcran.height;
+	int width = tailleEcran.width;
+	this.setSize(width/2, height/2);
+	this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         
-		boutonGO = new JButton("GO!");
-		boutonGO.setBackground(new Color(0, 0, 0));
+	boutonGO = new JButton("GO!");
+	boutonGO.setBackground(new Color(0, 0, 0));
         boutonGO.setFont(new Font("Stencil", 0, 48)); 
         boutonGO.setForeground(new Color(255, 255, 204));
         boutonGO.setText("GO!");
              
-		titre = new JLabel("HAPPY FRIGO");
+	titre = new JLabel("HAPPY FRIGO");
 		
-		titre.setFont(new Font("Stencil", 0, 48)); 
+	titre.setFont(new Font("Stencil", 0, 48)); 
         titre.setText("HAPPY FRI");
         
-		JPanel fond = new JPanel(new GridBagLayout());
-		fond.setBackground( new Color(135,206,250));
+	JPanel fond = new JPanel(new GridBagLayout());
+	fond.setBackground( new Color(135,206,250));
 		
 		
-		GridBagConstraints c= new GridBagConstraints();
+	GridBagConstraints c= new GridBagConstraints();
 		
 		
-		// branchement de l'écouteur
-		boutonGO.addActionListener(new EcouteurAccueil(this));
+	// branchement de l'écouteur
+	boutonGO.addActionListener(new EcouteurAccueil(this));
 
           
         //ajout du conteneur principal dans la fenetre
@@ -47,8 +51,8 @@ public class Accueil extends JFrame{
 
         //Ajout des éléments graphiques au conteneur principal		
 		
-		fond.add(titre);
-		fond.add(boutonGO);
+	fond.add(titre);
+	fond.add(boutonGO);
 		
 		
         //Rendre la fenêtre visible
