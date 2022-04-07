72d727a7b90e94de5f6918944c26e75c5b6d15bb
VinsHelo <102284459+VinsHelo@users.noreply.github.com>
2022-04-07 15:14:26 +0200

    Add files via upload

---
+++ Case.java
@@ -0,0 +1,69 @@
+import java.awt.*;    
+import javax.swing.*;    
+    
+public class Case extends JPanel{    
+	    
+  
+	// constructeur  
+	public Case(String jour, Color couleur){
+				
+		super(new BorderLayout());
+		
+		//contour noir de la case
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		// JPanel NORTH et CENTER de chaque case
+		JPanel fondJour = new JPanel();
+		JPanel recette = new JPanel();
+		
+		//on nomme la case
+		JLabel jourJ = new JLabel(jour);
+		
+		//police de caractere
+		jourJ.setFont(new Font("Stencil", 0, 24));
+		
+		//contour noir de chaque JPanel nom
+		fondJour.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		//mise en couleur du fondJour avec la couleur passée dans le constructeur
+		fondJour.setBackground(couleur); 
+		
+		//ajout du JLabel (nom du jour) à son fond (JPanel)
+		fondJour.add(jourJ);
+		
+		//ajout de fondJour dans la case
+		this.add(fondJour,BorderLayout.NORTH);
+		
+		recette.setLayout(new GridLayout(3,1));
+		
+		//création des différents sous panels qui iront dans le panel recette
+		JPanel casePetitDej = new JPanel(new BorderLayout()); 
+		JPanel caseDej = new JPanel(new BorderLayout());   
+		JPanel caseDiner = new JPanel(new BorderLayout()); 
+		
+		//création des différents labels soulignés qui iront dans les sous panels du panel recette
+		JLabel petitDej = new JLabel("<HTML><U>PETIT-DEJEUNER</U></HTML>",JLabel.CENTER);
+		JLabel dejeuner = new JLabel("<HTML><U>DEJEUNER</U></HTML>",JLabel.CENTER);
+		JLabel diner = new JLabel("<HTML><U>DINER</U></HTML>",JLabel.CENTER);
+		
+		//set d'une nouvelle police de caractère
+		petitDej.setFont(new Font("Stencil", 0, 18));
+		dejeuner.setFont(new Font("Stencil", 0, 18));
+		diner.setFont(new Font("Stencil", 0, 18));
+		
+		//ajout des labels à leurs panels respectifs
+		casePetitDej.add(petitDej,BorderLayout.NORTH);
+		caseDej.add(dejeuner,BorderLayout.NORTH);
+		caseDiner.add(diner,BorderLayout.NORTH);
+		
+		//ajout des différents sous panels dans le grand panel recette
+		recette.add(casePetitDej);
+		recette.add(caseDej);
+		recette.add(caseDiner);
+		
+		//ajout du panel recette dans ma case
+		this.add(recette,BorderLayout.CENTER);
+		
+		 
+	}
+}  
--- Semainier.java
+++ Semainier.java
@@ -1,121 +1,51 @@
 import java.awt.*;    
 import javax.swing.*;    
     
-public class Semainier{    
-	JFrame frameObj;    
+public class Semainier extends JFrame{      
   
 	// constructeur  
-	public Semainier(){    
-		frameObj = new JFrame();    		 
+	public Semainier(){     		 
 		
 		//récuperer la taille de l'écran + mettre fenetre au centre de l'ecran  + fermeture
-		frameObj.pack();
+		this.pack();
 		Dimension tailleEcran = Toolkit.getDefaultToolkit().getScreenSize();
 		int height = tailleEcran.height;
 		int width = tailleEcran.width;
-		frameObj.setSize(width, height);
-		frameObj.setLocationRelativeTo(null);
-        frameObj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+		this.setSize(width, height);
+		this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
         
-        //grandes cases (nom du jour + contenu recette)
-		JPanel case1 = new JPanel(new BorderLayout()); 
-		JPanel case2 = new JPanel(new BorderLayout());   
-		JPanel case3 = new JPanel(new BorderLayout());    
-		JPanel case4 = new JPanel(new BorderLayout());     
-		JPanel case5 = new JPanel(new BorderLayout());    
-		JPanel case6 = new JPanel(new BorderLayout());    
-		JPanel case7 = new JPanel(new BorderLayout());     
-		JPanel case8 = new JPanel(new BorderLayout());    
+        //création des différentes cases du planning
+        Case jourLundi = new Case("LUNDI", new Color(255, 153, 153));
+        Case jourMardi = new Case("MARDI", new Color(255, 204, 153));
+        Case jourMercredi = new Case("MERCREDI", new Color(255, 255, 153));
+        Case jourJeudi = new Case("JEUDI", new Color(204, 255, 153));
+        Case jourVendredi = new Case("VENDREDI", new Color(153, 255, 153));
+        Case jourSamedi = new Case("SAMEDI", new Color(153, 255, 204));
+        Case jourDimanche = new Case("DIMANCHE", new Color(153, 255, 255));
+        
+        //création de la dernière case qui contiendra le logo d'happyfrigo
+        JPanel case8 = new JPanel(new BorderLayout());    
 		case8.setBackground(new Color(192, 192, 192));
-		 
-		// faire contoure noir de chaque case
-		case1.setBorder(BorderFactory.createLineBorder(Color.black));
-		case2.setBorder(BorderFactory.createLineBorder(Color.black));
-		case3.setBorder(BorderFactory.createLineBorder(Color.black));
-		case4.setBorder(BorderFactory.createLineBorder(Color.black));
-		case5.setBorder(BorderFactory.createLineBorder(Color.black));
-		case6.setBorder(BorderFactory.createLineBorder(Color.black));
-		case7.setBorder(BorderFactory.createLineBorder(Color.black));
 		case8.setBorder(BorderFactory.createLineBorder(Color.black));
-		
-		// JPanel NORTH de chaque case (nom du jour)
-		JPanel fondLundi = new JPanel();
-		JPanel fondMardi = new JPanel();
-		JPanel fondMercredi = new JPanel();
-		JPanel fondJeudi = new JPanel();
-		JPanel fondVendredi = new JPanel();
-		JPanel fondSamedi = new JPanel();
-		JPanel fondDimanche = new JPanel();
-		
-		//on nomme les cases (JPanel nom du jour + future liste de courses)
-		JLabel lundi = new JLabel("LUNDI");
-		JLabel mardi = new JLabel("MARDI");
-		JLabel mercredi = new JLabel("MERCREDI");
-		JLabel jeudi = new JLabel("JEUDI");
-		JLabel vendredi = new JLabel("VENDREDI");
-		JLabel samedi = new JLabel("SAMEDI");
-		JLabel dimanche = new JLabel("DIMANCHE");
 		JLabel logo = new JLabel("HAPPYFRIGO", JLabel.CENTER);
-		
-		//polices de caractere
-		lundi.setFont(new Font("Stencil", 0, 24));
-		mardi.setFont(new Font("Stencil", 0, 24));
-		mercredi.setFont(new Font("Stencil", 0, 24));
-		jeudi.setFont(new Font("Stencil", 0, 24));
-		vendredi.setFont(new Font("Stencil", 0, 24));
-		samedi.setFont(new Font("Stencil", 0, 24));
-		dimanche.setFont(new Font("Stencil", 0, 24));
 		logo.setFont(new Font("Stencil", 0, 48));
-		
-		// faire contoure noir de chaque JPanel nom
-		fondLundi.setBorder(BorderFactory.createLineBorder(Color.black));
-		fondMardi.setBorder(BorderFactory.createLineBorder(Color.black));
-		fondMercredi.setBorder(BorderFactory.createLineBorder(Color.black));
-		fondJeudi.setBorder(BorderFactory.createLineBorder(Color.black));
-		fondVendredi.setBorder(BorderFactory.createLineBorder(Color.black));
-		fondSamedi.setBorder(BorderFactory.createLineBorder(Color.black));
-		fondDimanche.setBorder(BorderFactory.createLineBorder(Color.black));
-
-		fondLundi.setBackground(new Color(255, 153, 153));
-		fondMardi.setBackground(new Color(255, 204, 153));
-		fondMercredi.setBackground(new Color(255, 255, 153));
-		fondJeudi.setBackground(new Color(204, 255, 153));
-		fondVendredi.setBackground(new Color(153, 255, 153));
-		fondSamedi.setBackground(new Color(153, 255, 204));
-		fondDimanche.setBackground(new Color(153, 255, 255));
-		
-		//ajout des JLabel (nom du jour) a leurs fonds respectifs (JPanel)
-		fondLundi.add(lundi);
-		fondMardi.add(mardi); 
-		fondMercredi.add(mercredi);
-		fondJeudi.add(jeudi);
-		fondVendredi.add(vendredi);
-		fondSamedi.add(samedi);
-		fondDimanche.add(dimanche);
-		
-		//ajout des JPanel (fond colore) aux cases
-		case1.add(fondLundi,BorderLayout.NORTH);
-		case2.add(fondMardi,BorderLayout.NORTH);
-		case3.add(fondMercredi,BorderLayout.NORTH);
-		case4.add(fondJeudi,BorderLayout.NORTH);
-		case5.add(fondVendredi,BorderLayout.NORTH);
-		case6.add(fondSamedi,BorderLayout.NORTH);
-		case7.add(fondDimanche,BorderLayout.NORTH);
 		case8.add(logo,BorderLayout.CENTER);
-
-		// ajout des cases à la fenetre
-		frameObj.add(case1); 
-		frameObj.add(case2); 
-		frameObj.add(case3);  
-		frameObj.add(case4); 
-		frameObj.add(case5); 
-		frameObj.add(case6);  
-		frameObj.add(case7); 
-		frameObj.add(case8); 
-		  
-		// setting the grid layout using the parameterless constructor    
-		frameObj.setLayout(new GridLayout(2,4));
 		
-		frameObj.setVisible(true);    
+       //on set notre fenêtre en gridLayout pour avoir la disposition en forme de cases désirée     
+		this.setLayout(new GridLayout(2,4));
+		
+		// ajout des cases à la fenêtre
+		this.add(jourLundi); 
+		this.add(jourMardi); 
+		this.add(jourMercredi);  
+		this.add(jourJeudi); 
+		this.add(jourVendredi); 
+		this.add(jourSamedi);  
+		this.add(jourDimanche); 
+		this.add(case8); 
+		  
+		this.setVisible(true); 
+  
 	}
 }  
