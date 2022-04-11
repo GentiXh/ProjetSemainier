a8429e3faf03d78c2549b26d8c33e006054d486e
anoukaltmeyer <102285978+anoukaltmeyer@users.noreply.github.com>
2022-04-11 19:36:05 +0200

    Update Recette.java

--- Recette.java
+++ Recette.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class Recette extends JFrame {
 	
 	public Allergie fenAllergie;
+	public RegimeAlimentaire fenRegime;
 	private ArrayList<String> listeRecetteFrancais;
 	private ArrayList<String> listeRecettePetitDej;
 	private ArrayList<String> listeRecetteItalien;	
@@ -19,6 +20,9 @@ public class Recette extends JFrame {
 	private JPanel fastFood;
 	private JPanel petitDej;
 	private JPanel autreCategorie;
+	//public JButton  
+	
+	
 	
 	public Recette(){ //constructeur
 		
@@ -29,52 +33,51 @@ public class Recette extends JFrame {
 		int width = tailleEcran.width;
 		this.setSize(width/2, height/2);
 		this.setLocationRelativeTo(null);
-        	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-		//mettre en attributs
-		JPanel fond = new JPanel(new BorderLayout());
-		JLabel titre = new JLabel("RECETTES",JLabel.CENTER);
-		JTabbedPane jTabbedPane1 = new JTabbedPane();
-		francais = new JPanel();
+        //mettre en attributs
+        JPanel fond = new JPanel(new BorderLayout());
+        JLabel titre = new JLabel("RECETTES",JLabel.CENTER);
+        JTabbedPane jTabbedPane1 = new JTabbedPane();
+        francais = new JPanel();
 		italien = new JPanel();
-		asiatique = new JPanel();
-		fastFood = new JPanel();
-		petitDej = new JPanel();
-		autreCategorie = new JPanel();
-
-		fond.setBackground(new Color(204, 255, 204));
+        asiatique = new JPanel();
+        fastFood = new JPanel();
+        petitDej = new JPanel();
+        autreCategorie = new JPanel();
+        fenAllergie= new Allergie();
+        fenRegime = new RegimeAlimentaire();
+        
+        fond.setBackground(new Color(204, 255, 204));
 		this.add(fond);
 
-		titre.setBackground(new Color(255, 255, 255));
-		titre.setFont(new Font("Stencil", 0, 48)); 
-
+        titre.setBackground(new Color(255, 255, 255));
+        titre.setFont(new Font("Stencil", 0, 48)); 
+    
 		fond.add(titre, BorderLayout.NORTH);
+		
+        jTabbedPane1.setBackground(new Color(255, 255, 255));
+        fond.add(jTabbedPane1, BorderLayout.CENTER);
 
-		jTabbedPane1.setBackground(new Color(255, 255, 255));
-		fond.add(jTabbedPane1, BorderLayout.CENTER);
-
-
-		jTabbedPane1.addTab("Francais", francais);
-		jTabbedPane1.addTab("Italien", italien);     
-		jTabbedPane1.addTab("Asiatique", asiatique);
-		jTabbedPane1.addTab("Fast-food", fastFood);
-		jTabbedPane1.addTab("Petit dejeuner", petitDej);
-		jTabbedPane1.addTab("Autres", autreCategorie);
-
-
-		setVisible(true);
-
-
-		// lien avec BD
-
-		listeRecetteFastFood = new ArrayList<String>();
-		listeRecetteFrancais = new ArrayList<String>();
-		listeRecetteItalien = new ArrayList<String>();
-		listeRecetteAsiatique = new ArrayList<String>();
-		listeRecetteFastFood = new ArrayList<String>();
-		listeRecetteAutreCategorie = new ArrayList<String>();
-		listeRecettePetitDej = new ArrayList<String>(); 
-		TriDeContraintes t = new TriDeContraintes(fenAllergie.regime, fenAllergie.gluten, fenAllergie.oeuf, fenAllergie.lactose, fenAllergie.arachide, fenAllergie.fodmap, type); 
+        jTabbedPane1.addTab("Francais", francais);
+        jTabbedPane1.addTab("Italien", italien);     
+        jTabbedPane1.addTab("Asiatique", asiatique);
+        jTabbedPane1.addTab("Fast-food", fastFood);
+        jTabbedPane1.addTab("Petit dejeuner", petitDej);
+        jTabbedPane1.addTab("Autres", autreCategorie);
+        
+        setVisible(true);
+        
+        // lien avec BD
+        
+        listeRecetteFastFood = new ArrayList<String>();
+        listeRecetteFrancais = new ArrayList<String>();
+        listeRecetteItalien = new ArrayList<String>();
+        listeRecetteAsiatique = new ArrayList<String>();
+        listeRecetteFastFood = new ArrayList<String>();
+        listeRecetteAutreCategorie = new ArrayList<String>();
+        listeRecettePetitDej = new ArrayList<String>(); 
+		TriDeContraintes t = new TriDeContraintes(fenRegime.regime, fenAllergie.gluten, fenAllergie.oeuf, fenAllergie.lactose, fenAllergie.arachide, fenAllergie.fodmap); 
 		t.Tri();
 		listeRecetteFrancais = t.getListeFrancais();
 		listeRecettePetitDej = t.getListePetitDej();
@@ -82,6 +85,10 @@ public class Recette extends JFrame {
 		listeRecetteAsiatique = t.getListeAsiatique();
 		listeRecetteFastFood = t.getListeFastFood();
 		listeRecetteAutreCategorie = t.getListeAutreCategorie();
+        
+        // faire les checkbox dans chaque "sous fenetre" (cad type)
+        
+        
         
         
 	}
