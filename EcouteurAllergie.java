8eb589a5a3ea8e00f871ad4f4fa89b1a7a7870f8
GentiXh <80448947+GentiXh@users.noreply.github.com>
2022-04-16 12:41:27 +0200

    Update EcouteurAllergie.java

--- EcouteurAllergie.java
+++ EcouteurAllergie.java
@@ -5,20 +5,35 @@ public class EcouteurAllergie implements ActionListener{
 	
 	private Allergie fenetre;
 	private boolean resultat;
+	private ProfilUtilisateur profil;
 		
 	//constructeur
-	public EcouteurAllergie(Allergie fen, boolean res){
+	public EcouteurAllergie(Allergie fen, boolean res, ProfilUtilisateur p){
 		fenetre=fen;
 		resultat=res;
+		profil= p;
 	}
 	
 	public void actionPerformed(ActionEvent evt){
-		if(resultat==false){ //on a clique sur 'oui'
-			Recette f = new Recette();
-			f.setVisible(true);
-			fenetre.hide(); 			//pour cacher l'ancienne fenêtre
-		
-		}else if (resultat==true){ // on a clique sur 'non'
+		if(resultat==false){ 						//on a clique sur 'non'
+			Recette recette = new Recette(profil);
+			
+			//les allergens peuvent etre manges par l'utilisateur
+			profil.setGluten(true);
+			profil.setFodmap(true);
+			profil.setArachide(true);
+			profil.setLactose(true);
+			profil.setOeuf(true);
+			
+			//on affiche recette
+			recette.setVisible(true);
+			
+			//pour cacher l'ancienne fenêtre
+			fenetre.dispose(); 					
+				
+		}else if (resultat==true){					 //on a clique sur 'oui'
+			
+			//on refait la fenetre allergie
 			fenetre.centre.remove(fenetre.yesButton);
 			fenetre.centre.remove(fenetre.noButton);
 			fenetre.centre.remove(fenetre.allergieTexte);
@@ -28,8 +43,10 @@ public class EcouteurAllergie implements ActionListener{
 			fenetre.fodmapBox.setVisible(true);
 			fenetre.oeufBox.setVisible(true);
 			fenetre.revalidate();
-			fenetre.repaint(); 
+			fenetre.repaint();
 			fenetre.validerButton.setVisible(true);
 		}		
+		System.out.println("on est passe par ecouteur allergie");
+
 	}		
 }
