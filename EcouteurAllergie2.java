015a402f7e581ef9c89d101b743e7967edbc5cac
GentiXh <80448947+GentiXh@users.noreply.github.com>
2022-04-16 23:31:27 +0200

    Update EcouteurAllergie2.java

--- EcouteurAllergie2.java
+++ EcouteurAllergie2.java
@@ -3,54 +3,50 @@ import java.util.*;
 
 public class EcouteurAllergie2 implements ActionListener{
 	
-	private Allergie fenetre;
-	private int n1=2;
-	private int n2=2;
-	private int n3=2;
-	private int n4=2;
+	private Allergie fenetre;			//fenetre sur laquelle intervient cet ecouteur
+	
+	private int n1=2;					//pour permettre de savoir quand la checkbox est cochee ou non :
+	private int n2=2;					/*si nx modulo 2 vaut zero alors la case est cochee => l'utilisateur est allergique => boolean allergene = true*/
+	private int n3=2;					/*si nx modulo 2 vaut un alors la case n'est pas cochee => l'utilisateur n'est pas allergique => boolean allergene = false*/
+	private int n4=2;					/*chaque fois que l'on coche une meme checkbox nx augmente et la valeur de l'allergene change*/
 	private int n5=2;
-	/**private boolean g;
-	private boolean l;
-	private boolean a;
-	private boolean f;
-	private boolean o;*/
+
 	int allergene;
 	private ProfilUtilisateur profil;
 	
-	
 	//constructeur
 	public EcouteurAllergie2(Allergie fen, int val, ProfilUtilisateur p){
-		fenetre=fen;
+		fenetre = fen;
 		allergene = val;
-		profil =p;
+		profil = p;
 	}
 	
 	public void actionPerformed(ActionEvent evt) {
         if (allergene == 1){				//gluten
 			if (n1%2==0){
-				profil.setGluten(false);
-			}
-			else {
 				profil.setGluten(true);
 			}
-			System.out.println(" gluten ecouteur "+profil.gluten);
+			else {
+				profil.setGluten(false);
+			}
+			/**System.out.println(" gluten ecouteur "+profil.gluten);*/  //pour debug si besoin
 			n1++;
 		}
 		if (allergene == 2){				//lactose
 			if (n2%2==0){
-				profil.setLactose(false);
+				profil.setLactose(true);
 			}
 			else {
-				profil.setLactose(true);
+				profil.setLactose(false);
 			}
 			n2++;
 		}
 		if (allergene == 3){				//arachide
 			if (n3%2==0){
-				profil.setArachide(false);
+				profil.setArachide(true);
 			}
 			else {
-				profil.setArachide(true);
+				profil.setArachide(false);
 			}			
 			n3++;
 
@@ -58,31 +54,23 @@ public class EcouteurAllergie2 implements ActionListener{
 			
 		if (allergene == 4){				//fodmap
 			if (n4%2==0){
-				profil.setFodmap(false);
+				profil.setFodmap(true);
 			}
 			else {
-				profil.setFodmap(true);
+				profil.setFodmap(false);
 			}			
 			n4++;
 		}
 		
 		if (allergene == 5){				//oeuf
 			if (n5%2==0){
-				profil.setOeuf(false);
+				profil.setOeuf(true);
 			}
 			else {
-				profil.setOeuf(true);
+				profil.setOeuf(false);
 			}			
 			n5++;
 		}
-		System.out.println("on est passe par ecouteur allergie2");
-		
-		if (allergene ==-1){
-		Recette recette = new Recette(profil);
-		recette.setVisible(true);
-		//pour cacher l'ancienne fenêtre
-		fenetre.dispose();
-		}
-		System.out.println("on est passe par ecouteur valider");
+		/**System.out.println("on est passe par ecouteur allergie2");*/
     }
 }
