73882b3e74a2366042a4ec14209e79af59b377e2
GentiXh <80448947+GentiXh@users.noreply.github.com>
2022-04-16 12:44:39 +0200

    Update EcouteurValiderAllergie.java

--- EcouteurValiderAllergie.java
+++ EcouteurValiderAllergie.java
@@ -4,23 +4,20 @@ import java.util.*;
 public class EcouteurValiderAllergie implements ActionListener{
 	
 	private Allergie fenetre;
-
+	private ProfilUtilisateur profil;
 		
 	//constructeur
-	public EcouteurValiderAllergie(Allergie fen){
+	public EcouteurValiderAllergie(Allergie fen, ProfilUtilisateur p){
 		fenetre=fen;
+		profil = p;
 	}
 	
 	public void actionPerformed(ActionEvent evt) {
-        Recette f = new Recette();
-		f.setVisible(true);
-			
+        Recette recette = new Recette(profil);
+		recette.setVisible(true);
 		//pour cacher l'ancienne fenêtre
-		fenetre.hide();
+		fenetre.dispose();
+		System.out.println("on est passe par ecouteur valider");
     }
-	
-	
-    
-		
-		
 }
+//// INUTILE
