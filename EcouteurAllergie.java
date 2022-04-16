7002ac3757ef31b4819f1a3b671afdf683e17edb
GentiXh <80448947+GentiXh@users.noreply.github.com>
2022-04-16 23:30:19 +0200

    Update EcouteurAllergie.java

--- EcouteurAllergie.java
+++ EcouteurAllergie.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public class EcouteurAllergie implements ActionListener{
 	
-	private Allergie fenetre;
-	private boolean resultat;
-	private ProfilUtilisateur profil;
+	private Allergie fenetre; 			//fenetre sur laquelle intervient cet ecouteur
+	private boolean resultat;			//permet de distinguer sur quel bouton l'utilisateur a clique
+	private ProfilUtilisateur profil;	
 		
 	//constructeur
 	public EcouteurAllergie(Allergie fen, boolean res, ProfilUtilisateur p){
@@ -15,38 +15,35 @@ public class EcouteurAllergie implements ActionListener{
 	}
 	
 	public void actionPerformed(ActionEvent evt){
-		if(resultat==false){ 						//on a clique sur 'non'
-			Recette recette = new Recette(profil);
-			
-			//les allergens peuvent etre manges par l'utilisateur
-			profil.setGluten(true);
-			profil.setFodmap(true);
-			profil.setArachide(true);
-			profil.setLactose(true);
-			profil.setOeuf(true);
+		if(resultat==false){ 							 //on a clique sur 'non'
+			Recette recette = new Recette(profil);       /*appel la classe recette en passant l'instance de ProfilUtilisateur*/
 			
 			//on affiche recette
-			recette.setVisible(true);
+			recette.setVisible(true);					
 			
 			//pour cacher l'ancienne fenêtre
-			fenetre.dispose(); 					
+			fenetre.dispose(); 			
+			//Explication: les valeurs d'allergies ont ete mises a false par defaut dans la classe Allergie, donc pas besoin de faire appel aux setteurs de ProfilUtilisateur pour mettre tout en false		
 				
-		}else if (resultat==true){					 //on a clique sur 'oui'
+		}else if (resultat==true){						 //on a clique sur 'oui'
 			
 			//on refait la fenetre allergie
+			/*on enleve les boutons 'oui' et 'non' et le label*/
 			fenetre.centre.remove(fenetre.yesButton);
 			fenetre.centre.remove(fenetre.noButton);
 			fenetre.centre.remove(fenetre.allergieTexte);
+			/*on affiche les checkbox et le bouton valider*/
 			fenetre.glutenBox.setVisible(true);
 			fenetre.lactoseBox.setVisible(true);
 			fenetre.arachideBox.setVisible(true);
 			fenetre.fodmapBox.setVisible(true);
 			fenetre.oeufBox.setVisible(true);
+			fenetre.validerButton.setVisible(true);
+			
 			fenetre.revalidate();
 			fenetre.repaint();
-			fenetre.validerButton.setVisible(true);
+			
 		}		
-		System.out.println("on est passe par ecouteur allergie");
-
+		/**System.out.println("on est passe par ecouteur allergie");*/
 	}		
 }
