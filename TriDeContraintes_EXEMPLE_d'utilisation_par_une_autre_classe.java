97d2f5244932d3ac1b1c218ea33d0c6ad96646e2
GentiXh <80448947+GentiXh@users.noreply.github.com>
2022-04-16 12:51:47 +0200

    Delete TriDeContraintes_EXEMPLE_d'utilisation_par_une_autre_classe.java

--- TriDeContraintes_EXEMPLE_d'utilisation_par_une_autre_classe.java
+++
@@ -1,4 +0,0 @@
-Arraylist<String> listeRecette = new Arraylist<String>; // private et puis definie sur le constructeur de la classe
-TriDeContraintes t = new TriDeContraintes(int regime, ......);
-t.Tri();
-listeRecette = t.getListe1();
