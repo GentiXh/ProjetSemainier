19dc3f74f3799458c4060b508418a129e34b8e10
GentiXh <80448947+GentiXh@users.noreply.github.com>
2022-04-17 00:08:24 +0200

    Update TriDeContraintes.java

--- TriDeContraintes.java
+++ TriDeContraintes.java
@@ -5,16 +5,18 @@ import java.util.*;
 
 public class TriDeContraintes{
 	
-	// les choix de l'utilisateur
-	private int regime; // 0 pour omnivore, 1 pour vegetarien et 2 pour vegan
-	// pour tous les allergenes : false = ne contient pas l'allergene et donc l'utilisatuer allergique a cela peut en manger
-	private boolean gluten; //1 => pas allergique au gluten		//0 => allergique au gluten
+	// les choix de l'utilisateur:
+	
+	private int regime; 			// 0 pour omnivore, 1 pour vegetarien et 2 pour vegan
+	
+	// pour tous les allergenes : true => l'utilisateur est allergique a l'allergene en question => le code va cherche a exclure les ingredients qui contient cet allergene
+	private boolean gluten;
 	private boolean oeuf;
 	private boolean lactose;
 	private boolean arachide;
 	private boolean fodmap;
 	
-	//les strings des commandes sql qui changent selon les choix de l'utilisateur (voir a partir de ligne 44)
+	//les strings des commandes sql qui changent selon les choix de l'utilisateur (voir a partir de ligne 47)
 	private String Regime;   
 	private String Gluten;
 	private String Oeuf;
@@ -22,64 +24,72 @@ public class TriDeContraintes{
 	private String Arachide;
 	private String Fodmap;
 	
-	private String type ; 
+	//listes (qui seront remplies sur cette classe) selon les types des plats proposees par le query de tri
 	
-	public ArrayList<String> listeRecettePetitDej; //repas petit dej
-	public ArrayList<String> listeRecetteFrancais; //repas francais
-	public ArrayList<String> listeRecetteItalien; //repas italien
-	public ArrayList<String> listeRecetteAsiatique; //repas asiatique
-	public ArrayList<String> listeRecetteFastFood; //repas fast food
-	public ArrayList<String> listeRecetteAutreCategorie;  //repas autre categorie
+	private ArrayList<String> listeRecettePetitDej; 			//repas petit dej
+	private ArrayList<String> listeRecetteFrancais; 			//repas francais
+	private ArrayList<String> listeRecetteItalien; 				//repas italien
+	private ArrayList<String> listeRecetteAsiatique;			//repas asiatique
+	private ArrayList<String> listeRecetteFastFood; 			//repas fast food
+	private ArrayList<String> listeRecetteAutreCategorie;    	//repas autre categorie
 	
 	//constructeur
 	public TriDeContraintes(ProfilUtilisateur p){
 		
-		this.regime = p.regime;
-		this.gluten = p.gluten;
-		this.oeuf = p.oeuf;
-		this.lactose = p.lactose;
-		this.arachide = p.arachide;
-		this.fodmap = p.fodmap; 
+		//association des valeurs enregistrees aux variables de cette classe
+		this.regime = p.getRegime();
+		this.gluten = p.getGluten();
+		this.oeuf = p.getOeuf();
+		this.lactose = p.getLactose();
+		this.arachide = p.getArachide();
+		this.fodmap = p.getFodmap(); 
 		
-		Regime = "i.regime = "+regime;									//si vegan, cherche que les recettes veganes
-		if (regime == 0){												//si omnivore
-			 Regime = "("+Regime+" OR i.regime = 1 OR i.regime = 2)";	//inclut les recettes veganes et vege aussi
+		//dans les commentaires suivants, le terme "exclut" sera utilisee car la query contient un NOT IN afin d'exclure les "compopsitions" recette+ingredient qui ne marche pas pour faire le tri selon les donnees de ProfilUtilisateur
+								
+		if (regime == 0){										//si omnivore :
+			 Regime = " and ((i.regime = 3)";					/*exclut les ingredients de regime 3. regime 3 n'est associee a rien, donc n'exclut rien (puisque omnivore comprend tout les regime)*/
+		}														/*On a du faire cela et ne pas dire Regime = "" car sinon la requete query ne marche pas dans certain cas: des and/or et parantheses en plus ou moins que necessaires ce qui cree des erreurs de syntaxe ou resultats incoherents*/
+	
+		if (regime == 1){										//si vegetarien :
+			 Regime = " and ((i.regime = 0) ";					/*exclut les recettes omnivores*/
 		}
-		if (regime == 1){												//si vege
-			 Regime = "("+Regime+" OR i.regime = 2)";					//inclut les recettes veganes aussi
+		if (regime == 2){										//si vegan :
+			Regime = " and ((i.regime = 1 or i.regime = 0) ";	/*exclut les recettes omnivores et vegetariennes*/
 		}
 		
-		Gluten = "i.gluten = "+gluten;					//si false, on est allergique au gluten, c-a-d qu'on cherche que les ingredients qui ont l'attribut de gluten comme false, donc qui n'ont pas de gluten
-		if (gluten == true){ 							//si pas allergique au gluten...
-			Gluten = "("+Gluten+" OR i.gluten = 0)";	//...prends en comptes les ingredients qui ont du gluten OU qui n'ont pas du gluten
+		Gluten = " or i.gluten = "+gluten;						//si true, on est allergique au gluten, query va chercher a exclure les ingredients qui contient du gluten
+		if (gluten == false){ 									//si pas allergique au gluten :
+			Gluten = "";										/*n'exclut rien en se basant sur l'existence ou non du gluten dans l'ingredient en question*/
 		}
 
-		Oeuf = "i.oeuf = "+oeuf;					
-		if (oeuf == true){ 							
-			Oeuf = "("+Oeuf+" OR i.oeuf = 0)";	
+		Oeuf = " or i.oeuf = "+oeuf;							//idem			
+		if (oeuf == false){ 							
+			Oeuf = "";	
 		}	
 		
-		Lactose = "i.lactose = "+lactose;					
-		if (lactose == true){ 							
-			Lactose = "("+Lactose+" OR i.lactose = 0)";	
+		Lactose = " or i.lactose = "+lactose;					//idem	
+		if (lactose == false){ 							
+			Lactose = "";	
 		}	
 		
-		Arachide = "i.arachide = "+arachide;					
-		if (arachide == true){ 							
-			Arachide = "("+Arachide+" OR i.arachide = 0)";	
+		Arachide = " or i.arachide = "+arachide;				//idem		
+		if (arachide == false){ 							
+			Arachide = "";	
 		}
 		
-		Fodmap = "i.fodmap = "+fodmap;					
-		if (fodmap == true){ 							
-			Fodmap = "("+Fodmap+" OR i.fodmap = 0)";	
+		Fodmap = " or i.fodmap = "+fodmap;						//idem	
+		if (fodmap == false){ 							
+			Fodmap = "";	
 		}			
-		
 	}
 	
 	public void Tri(){
+		
+		//connection a la base de donnees
 		Connection conDB = null; 
 		Statement statement = null;
 		ResultSet query = null;
+		
 		// initialise recette 
 		listeRecettePetitDej = new ArrayList<String>(); 
 		listeRecetteFrancais = new ArrayList<String>();
@@ -96,61 +106,37 @@ public class TriDeContraintes{
 				statement = conDB.createStatement();
 				
 				//requetes SQL
-				query = statement.executeQuery("select r.type, r.nomRecette, count(c.idIngredient) from recettes r, compositionrecette c, ingredients i where r.idRecette = c.idRecette and i.idIngredient = c.idIngredient and "+Regime+" and "+Gluten+" and "+Lactose+" and "+Oeuf+" and "+Fodmap+" and "+Arachide+" group by c.idRecette having count(c.idIngredient) in (select count(c.idIngredient) from recettes r, compositionrecette c, ingredients i where r.idRecette = c.idRecette and i.idIngredient = c.idIngredient group by c.idRecette)");
-				//pour recettes diff de petits dejeuners
-				while(query.next()){
-					
-					if (!listeRecettePetitDej.contains(query.getString("r.nomRecette")) && !listeRecetteFrancais.contains(query.getString("r.nomRecette")) && !listeRecetteItalien.contains(query.getString("r.nomRecette")) && !listeRecetteAsiatique.contains(query.getString("r.nomRecette")) && !listeRecetteFastFood.contains(query.getString("r.nomRecette")) && !listeRecetteAutreCategorie.contains(query.getString("r.nomRecette"))){  // pour eviter doublons
-						
-						switch(query.getString("r.type")){
-							
-							case "petitDej":
-								listeRecettePetitDej.add(query.getString("r.nomRecette"));
-								break;
-								
-							case "francais":
-								listeRecetteFrancais.add(query.getString("r.nomRecette"));
-								break;
-								
-							case "italien":
-								listeRecetteItalien.add(query.getString("r.nomRecette"));
-								break;
-								
-							case "asiatique":
-								listeRecetteAsiatique.add(query.getString("r.nomRecette"));
-								break;
-								
-							case "fastFood":
-								listeRecetteFastFood.add(query.getString("r.nomRecette"));
-								break;
-								
-							case "autreCategorie":
-								listeRecetteAutreCategorie.add(query.getString("r.nomRecette"));
-								break;
-							
-						}	
-					}
-				}
+				query = statement.executeQuery("select distinct r.`type`, r.nomRecette from recettes r, compositionrecette c, ingredients i where r.idRecette=c.idRecette and i.idIngredient=c.idIngredient and c.idRecette not in (select c.idRecette from ingredients i, recettes r, compositionrecette c where r.idRecette=c.idRecette and i.idIngredient=c.idIngredient"+Regime+Gluten+Lactose+Fodmap+Oeuf+Arachide+"))");
 				
-			/*	for (int i = 0; i<listeRecettePetitDej.size(); i++){ // affichage dans console des repas pour debug
-					System.out.println(listeRecettePetitDej.get(i));
+				while(query.next()){
+											
+					switch(query.getString("r.type")){		//ajoute les recettes triees dans les listes de types de plats correspondants
+							
+						case "petitDej":
+							listeRecettePetitDej.add(query.getString("r.nomRecette"));
+							break;
+								
+						case "francais":
+							listeRecetteFrancais.add(query.getString("r.nomRecette"));
+							break;
+								
+						case "italien":
+							listeRecetteItalien.add(query.getString("r.nomRecette"));
+							break;
+								
+						case "asiatique":
+							listeRecetteAsiatique.add(query.getString("r.nomRecette"));
+							break;
+								
+						case "fastFood":
+							listeRecetteFastFood.add(query.getString("r.nomRecette"));
+							break;
+								
+						case "autreCategorie":
+							listeRecetteAutreCategorie.add(query.getString("r.nomRecette"));
+							break;		
+					}	
 				}
-				for (int i = 0; i<listeRecetteFrancais.size(); i++){ // affichage dans console des repas pour debug
-					System.out.println(listeRecetteFrancais.get(i));
-				}
-				for (int i = 0; i<listeRecetteItalien.size(); i++){ // affichage dans console des repas pour debug
-					System.out.println(listeRecetteItalien.get(i));
-				}
-				for (int i = 0; i<listeRecetteAsiatique.size(); i++){ // affichage dans console des repas pour debug
-					System.out.println(listeRecetteAsiatique.get(i));
-				}
-				for (int i = 0; i<listeRecetteFastFood.size(); i++){ // affichage dans console des repas pour debug
-					System.out.println(listeRecetteAsiatique.get(i));
-				}
-				for (int i = 0; i<listeRecetteAutreCategorie.size(); i++){ // affichage dans console des repas pour debug
-					System.out.println(listeRecetteAutreCategorie.get(i));
-				}
-			*/
 				
 			} catch (Exception ex) {ex.printStackTrace();}  // pour retrouver sources des erreurs eventuelles
 			finally{
@@ -169,7 +155,7 @@ public class TriDeContraintes{
 			}
 		}
 		
-		public void effacerContraintes(){ //vider les listes
+		public void effacerContraintes(){ 				//vider les listes si besoin/debug
 			listeRecettePetitDej.clear();
 			listeRecetteFrancais.clear();
 			listeRecetteItalien.clear();
@@ -178,27 +164,29 @@ public class TriDeContraintes{
 			listeRecetteAutreCategorie.clear();
 		}
 		
-		public ArrayList <String> getListePetitDej() {
-			return listeRecettePetitDej;
+		//getteurs des listes de recettes selon les types
+		
+		public ArrayList <String> getItalienList(){
+			return new ArrayList<String>(listeRecetteItalien);
 		}
 		
-		public ArrayList <String> getListeItalien(){
-			return listeRecetteItalien;
+		public ArrayList <String> getFrancaisList(){
+			return new ArrayList<String>(listeRecetteFrancais);
 		}
 		
-		public ArrayList <String> getListeFrancais() {
-			return listeRecetteFrancais;
+		public ArrayList <String> getAsiatiqueList(){
+			return new ArrayList<String>(listeRecetteAsiatique);
 		}
 		
-		public ArrayList <String> getListeAsiatique() {
-			return listeRecetteAsiatique;
+		public ArrayList <String> getFastFoodList(){
+			return new ArrayList<String>(listeRecetteFastFood);
 		}
 		
-		public ArrayList <String> getListeFastFood() {
-			return listeRecetteFastFood;
+		public ArrayList <String> getAutreCategorieList(){
+			return new ArrayList<String>(listeRecetteAutreCategorie);
 		}
-		public ArrayList <String> getListeAutreCategorie() {
-			return listeRecetteAutreCategorie;
+		
+		public ArrayList <String> getPetitDejList(){
+			return new ArrayList<String>(listeRecettePetitDej);
 		}
 }
-
