b7321e3eba250448e475f334ebbc2c746654ca38
GentiXh <80448947+GentiXh@users.noreply.github.com>
2022-04-18 23:24:01 +0200

    Create Courses.java

---
+++ Courses.java
@@ -0,0 +1,96 @@
+import java.awt.*;    
+import javax.swing.*;    
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.*;
+import java.util.*;
+
+public class Courses extends JFrame{      
+  
+	private ProfilUtilisateur profil;
+	private ArrayList<String> listeCourses; //liste des courses des ingredients pour les recettes choisies
+	// constructeur  
+	public Courses(ProfilUtilisateur p){     		 
+		super("Liste de Courses");
+		
+		profil = p;
+		listeCourses = new ArrayList<String>();
+		//récuperer la taille de l'écran + mettre fenetre au centre de l'ecran  + fermeture
+		this.pack();
+		Dimension tailleEcran = Toolkit.getDefaultToolkit().getScreenSize();
+		int height = tailleEcran.height;
+		int width = tailleEcran.width;
+		this.setSize(width/2, height/2);
+		this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        
+        
+        JPanel fond = new JPanel(new BorderLayout());
+        JLabel titre = new JLabel("Liste de courses :",JLabel.CENTER);
+        
+        
+        fond.setBackground(new Color(252, 178, 178));
+        titre.setFont(new java.awt.Font("Stencil", 0, 36)); 
+        
+        
+        //ajout des éléments dans la fenetre
+        
+        //mettre la liste de courses dans le CENTER !!
+        fond.add(titre, BorderLayout.NORTH);
+		this.add(fond);
+		
+		creerListeCourses();
+		this.setLayout(new GridLayout(listeCourses.size(),1));
+		
+		for(int i=0;i<listeCourses.size();i++){
+			
+			JLabel ingredient = new JLabel(listeCourses.get(i));
+						
+			ingredient.setFont(new Font("Stencil", 0, 24));	
+			
+			this.add(ingredient);
+                
+        }  
+		
+		//affichage de la fenetre 
+		this.setVisible(true); 
+	}
+	
+	public void creerListeCourses(){
+		Connection conDB = null; 
+		Statement statement = null;
+		ResultSet query = null;
+		try{
+			Class.forName("org.mariadb.jdbc.Driver"); // on connecte au driver de la MariaBD
+            System.out.println("Driver trouve...");
+			conDB = DriverManager.getConnection("jdbc:mariadb://localhost:3306/ProjetSemainier", "root", "root"); // on connecte a la BD
+				statement = conDB.createStatement();
+				
+				for (int i = 0; i<profil.getRepas().size(); i++){			//parcours de toutes les recettes choisis
+					String s = profil.getRepas().get(i);
+					//requetes SQL
+					query = statement.executeQuery("select distinct i.nomIngredient from recettes r, compositionrecette c, ingredients i where r.idRecette=c.idRecette and i.idIngredient=c.idIngredient and r.nomRecette = '"+s+"'");					
+					while(query.next()){
+						if(!listeCourses.contains(query.getString("i.nomIngredient"))){ //ici le "distinct" ne suffit par car on va parcourir la bd plusieurs fois avec le for
+						listeCourses.add(query.getString("i.nomIngredient"));
+						}
+					}
+				}
+				
+			} catch (Exception ex) {ex.printStackTrace();}  // pour retrouver sources des erreurs eventuelles
+			finally{
+				try{query.close();}
+				catch (Exception e){
+					e.printStackTrace();
+				}
+				try {statement.close();}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+				try{conDB.close();}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+			}	
+	}
+}  
