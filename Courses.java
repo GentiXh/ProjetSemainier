3fb6216c23fd508dfaea76fbfad5b22b45c2900e
anoukaltmeyer <102285978+anoukaltmeyer@users.noreply.github.com>
2022-04-24 14:44:15 +0200

    Update Courses.java

--- Courses.java
+++ Courses.java
@@ -26,43 +26,46 @@ public class Courses extends JFrame{
 		this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
         
+        //creation des elements graphiques
+        
+        //parametrage du bouton retour
         JButton btnSemainier = new JButton("Retour au semainier");
 		btnSemainier.setBackground(new Color(0, 0, 0));
 		btnSemainier.setFont(new Font("Stencil", 0, 40));
 		btnSemainier.setForeground(new Color(252, 178, 178));
-		btnSemainier.addActionListener(new EcouteurCourses(this,profil));		       
-        
-        JPanel fond = new JPanel(new BorderLayout());
-        JPanel milieu = new JPanel();
-        JPanel bas = new JPanel();
-        milieu.setLayout(new GridLayout(listeCourses.size(),1));
-        
+		btnSemainier.addActionListener(new EcouteurCourses(this,profil));
+				       
+        //parametrage des panels
         JLabel titre = new JLabel("Liste de courses :",JLabel.CENTER);
-        
-        bas.add(btnSemainier); 
+        JPanel fond = new JPanel(new BorderLayout());
+        JPanel bas = new JPanel();
+        bas.add(btnSemainier);
+        JPanel milieu = new JPanel();
+        milieu.setLayout(new GridLayout(listeCourses.size(),1));
+        JScrollPane scroll = new JScrollPane(milieu, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED); //Il y aura toujours un bouton pour defiler verticalement et un horizontal quand cela est necessaire seulement
+        scroll.setBackground (new Color (252, 178,178)); //apparement ne sert a rien
+        scroll.setViewportView(milieu);      
         
         fond.setBackground(new Color(252, 178, 178));
         milieu.setBackground(new Color(252, 178, 178));
 		bas.setBackground(new Color(252, 178, 178));
         titre.setFont(new java.awt.Font("Stencil", 0, 36)); 
 
-		creerListeCourses();   //appelle la methode au dessous qui trouve les ingredients de recette selectionnees et gardees par ProfilUtilisateur
-		
-		for(int i=0;i<listeCourses.size();i++){
-			
-			JLabel ingredient = new JLabel("- "+listeCourses.get(i));
-						
-			ingredient.setFont(new Font("Stencil", 0, 24));	
-			
-			milieu.add(ingredient);
-                
+		creerListeCourses();   //appelle la methode au-dessous qui trouve les ingredients de recette selectionnees et gardees par ProfilUtilisateur
+
+		// on ajoute les ingredients a l'affichage
+		for(int i=0;i<listeCourses.size();i++){			
+			JLabel ingredient = new JLabel("- "+listeCourses.get(i),JLabel.CENTER);				
+			ingredient.setFont(new Font("Stencil", 0, 24));				
+			milieu.add(ingredient);                
         }  
 		
 		//ajout des éléments dans la fenetre
         fond.add(titre, BorderLayout.NORTH);
-        fond.add(milieu, BorderLayout.CENTER);
         fond.add(bas, BorderLayout.SOUTH);
+		fond.add(scroll);
 		this.add(fond);
+		
 		
 		//affichage de la fenetre 
 		this.setVisible(true); 
